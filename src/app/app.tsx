5fc9ddfbda9d0fef85815ece749353f99151de1b
raksh3011 <rakshak.km.3011@gmail.com>
2026-01-31 17:31:48 +0530

    login2

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
+import { AuthProvider } from './src/context/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar barStyle="dark-content" />
+    <AuthProvider>
       <AppNavigator />
-    </SafeAreaView>
+    </AuthProvider>
   );
 }
